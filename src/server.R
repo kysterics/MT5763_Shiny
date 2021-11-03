a7794ef07e4ddf7c2ea94b3370b89740301ada7a
kysterics <63026996+kysterics@users.noreply.github.com>
2021-11-03 14:38:28 +0000

    Set up test table with download button.

--- src/server.R
+++ src/server.R
@@ -1,10 +1,29 @@
 server <- function(input, output) {
-  paths_allowed("https://www.amazon.co.uk/")
+  # paths_allowed("https://www.amazon.co.uk/")
   
   
-  txt1 <- eventReactive(input$button, {input$txt1})
-  txt2 <- eventReactive(input$button, {input$txt2})
-  output$txtout <- renderText({
-    paste(txt1(), txt2(), sep = " ")
+  data <- eventReactive(input$button, {
+    # Web page containing data over several weeks
+    #URL <- "https://www.skysports.com/premier-league-table"
+    URL <- "https://uk.investing.com/crypto/bitcoin/historical-data"
+    
+    # Download the whole web page
+    htmlPage <- read_html(URL) 
+    
+    # Access the whole table
+    marketTable <- htmlPage %>%
+      #html_nodes(".standing-table__table") %>%
+      html_nodes("#curr_table") %>%
+      html_table
+
   })
+
+  # txt1 <- eventReactive(input$button, {input$txt1})
+  # txt2 <- eventReactive(input$button, {input$txt2})
+  
+  
+  output$tableOut <- renderTable({
+    data()
+  })
+  
 }
